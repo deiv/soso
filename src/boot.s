20a3c09f809925ab2c68a6b1822a3f9d3d8450f8
deiv <oxia.eikichi@gmail.com>
2012-04-30 04:27:34 +0200

    Some changes to boot.s.

--- src/boot.s
+++ src/boot.s
@@ -1,17 +1,17 @@
 ;
 ; boot.s -- Kernel start location. Also defines multiboot header.
-;           Based on Bran's kernel development tutorial file start.asm
+;
+;           Based on JamesM's kernel development tutorials.
 ;
 
-MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
-MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
-MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
-; NOTE: We do not use MBOOT_AOUT_KLUDGE. It means that GRUB does not
-; pass us a symbol table.
-MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
-MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+MULTIBOOT_PAGE_ALIGN    equ 1<<0        ; Load kernel and modules on a page boundary.
+MULTIBOOT_MEM_INFO      equ 1<<1        ; Provide your kernel with memory info.
+MULTIBOOT_HEADER_MAGIC  equ 0x1BADB002  ; Multiboot Magic value.
 
-KERNEL_MAGIC		equ	0x13265213 ; Our magic number
+MULTIBOOT_HEADER_FLAGS  equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEM_INFO
+MULTIBOOT_CHECKSUM      equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+KERNEL_MAGIC		    equ	0x13265213  ; Our magic number.
 
 [BITS 32]                       ; All instructions should be 32-bit.
 
@@ -20,44 +20,47 @@ KERNEL_MAGIC		equ	0x13265213 ; Our magic number
 [EXTERN bss]                    ; Start of the .bss section.
 [EXTERN end]                    ; End of the last loadable section.
 
+; Multiboot header.
 mboot:
-    dd  MBOOT_HEADER_MAGIC      ; GRUB will search for this value on each
-                                ; 4-byte boundary in your kernel file
-    dd  MBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings
-    dd  MBOOT_CHECKSUM          ; To ensure that the above values are correct
+    dd  MULTIBOOT_HEADER_MAGIC      ; GRUB will search for this value on each
+                                ; 4-byte boundary in your kernel file.
+    dd  MULTIBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings.
+    dd  MULTIBOOT_CHECKSUM          ; To ensure that the above values are correct.
     
-    dd  mboot                   ; Location of this descriptor
+    dd  mboot                   ; Location of this descriptor.
     dd  code                    ; Start of kernel '.text' (code) section.
     dd  bss                     ; End of kernel '.data' section.
     dd  end                     ; End of kernel.
     dd  start                   ; Kernel entry point (initial EIP).
 
 [GLOBAL start]                  ; Kernel entry point.
-[EXTERN main]                   ; This is the entry point of our C code
+[EXTERN main]                   ; This is the entry point of our C code.
 
 start:
-	; check if data segment linked, located, and loaded properly
-	mov eax,[kernel_magic]
-	cmp eax,KERNEL_MAGIC
-	je kernel_ok
+	; Check if data segment linked, located, and loaded properly.
+	mov eax, [kernel_magic]
+	cmp eax, KERNEL_MAGIC
+	jne die
 
-	; display a blinking white-on-blue 'D' and freeze
-	mov word [0B8000h],9F44h
-	jmp short $
-kernel_ok:
+    push esp                    ; Pass the stack pointer.
+    push ebx                    ; And the multiboot header to main().
 
-    ; Load multiboot information:
-    push esp
-    push ebx
 
     ; Execute the kernel:
     cli                         ; Disable interrupts.
     call main                   ; call our main() function.
-    jmp $                       ; Enter an infinite loop, to stop the processor
+    jmp $                       ; Enter an infinite loop, to stop the processor.
                                 ; executing whatever rubbish is in the memory
                                 ; after our kernel!
+								
+die:
+	; Display a blinking white-on-blue 'D' and freeze.
+    ; TODO: better die message...
+	mov word [0B8000h],9F44h
+	jmp short $
 
 SECTION .data
 
 kernel_magic:
 	dd KERNEL_MAGIC
+
